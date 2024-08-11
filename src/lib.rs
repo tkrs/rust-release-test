51afca1148e0ea8d415b51d70184afd84ed124eb
Takeru Sato <type.in.type@gmail.com>
2024-08-11 23:25:10 +0900

    feat: add minus function

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,23 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn minus(left: i64, right: i64) -> i64 {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn add_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn minus_works() {
+        let result = minus(2, 3);
+        assert_eq!(result, -1);
     }
 }
